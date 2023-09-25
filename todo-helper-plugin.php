5bc639041df4a320160218111ead991bfd85946f
Henri Tikkanen <64799100+henritik@users.noreply.github.com>
2023-09-25 20:53:46 +0300

    Removed register_meta_box_cb

--- todo-helper-plugin.php
+++ todo-helper-plugin.php
@@ -14,7 +14,7 @@ Text Domain: todo
 defined( 'ABSPATH' ) or die();
 
 function todo_load_textdomain() {
-  load_plugin_textdomain( 'todo', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' ); 
+	load_plugin_textdomain( 'todo', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' ); 
 }
 add_action( 'init', 'todo_load_textdomain' );
 
@@ -35,17 +35,14 @@ function todo_create_cpt() {
 
 	$args = array (
 		'labels' 		=> $labels,
-    'description'		=> 'To-do list',
+		'description'		=> 'To-do list',
 		'public' 		=> true,
 		'show_in_nav_menus' 	=> false,
 		'menu_icon' 		=> 'dashicons-list-view',
 		'supports' 		=> array( 'title' ),
-		'show_in_rest' 		=> true,
-		'register_meta_box_cb' => 'todo_add_custom_box'
+		'show_in_rest' 		=> true
 	);
-
 	register_post_type( 'to-do', $args );
-
 }
 add_action( 'init', 'todo_create_cpt' );
 
@@ -53,33 +50,33 @@ add_action( 'init', 'todo_create_cpt' );
 
 function todo_create_taxonomy() {
  
-  $labels = array(
-    'name' => _x( 'States', 'taxonomy general name', 'todo' ),
-    'singular_name' => _x( 'State', 'taxonomy singular name', 'todo' ),
-    'search_items' =>  __( 'Search States', 'todo' ),
-    'popular_items' => __( 'Popular States', 'todo' ),
-    'all_items' => __( 'All States', 'todo' ),
-    'parent_item' => null,
-    'parent_item_colon' => null,
-    'edit_item' => __( 'Edit State', 'todo' ), 
-    'update_item' => __( 'Update State', 'todo' ),
-    'add_new_item' => __( 'Add New State', 'todo' ),
-    'new_item_name' => __( 'New State Name', 'todo' ),
-    'separate_items_with_commas' => __( 'Separate states with commas', 'todo' ),
-    'add_or_remove_items' => __( 'Add or remove states', 'todo' ),
-    'choose_from_most_used' => __( 'Choose from the most used states', 'todo' ),
-    'menu_name' => __( 'States', 'todo' ),
-  ); 
+$labels = array(
+	'name' => _x( 'States', 'taxonomy general name', 'todo' ),
+	'singular_name' => _x( 'State', 'taxonomy singular name', 'todo' ),
+	'search_items' =>  __( 'Search States', 'todo' ),
+	'popular_items' => __( 'Popular States', 'todo' ),
+	'all_items' => __( 'All States', 'todo' ),
+	'parent_item' => null,
+	'parent_item_colon' => null,
+	'edit_item' => __( 'Edit State', 'todo' ), 
+	'update_item' => __( 'Update State', 'todo' ),
+	'add_new_item' => __( 'Add New State', 'todo' ),
+	'new_item_name' => __( 'New State Name', 'todo' ),
+	'separate_items_with_commas' => __( 'Separate states with commas', 'todo' ),
+	'add_or_remove_items' => __( 'Add or remove states', 'todo' ),
+	'choose_from_most_used' => __( 'Choose from the most used states', 'todo' ),
+	'menu_name' => __( 'States', 'todo' ),
+); 
  
-  register_taxonomy('states','to-do',array(
-    'hierarchical' => false,
-    'labels' => $labels,
-    'show_ui' => true,
-    'show_in_rest' => true,
-    'show_admin_column' => true,
-    'update_count_callback' => '_update_post_term_count',
-    'query_var' => true,
-    'rewrite' => array( 'slug' => 'topic' ),
-  ));
+register_taxonomy('states','to-do',array(
+	'hierarchical' => false,
+	'labels' => $labels,
+	'show_ui' => true,
+	'show_in_rest' => true,
+	'show_admin_column' => true,
+	'update_count_callback' => '_update_post_term_count',
+	'query_var' => true,
+	'rewrite' => array( 'slug' => 'topic' ),
+	));
 }
 add_action( 'init', 'todo_create_taxonomy' );
